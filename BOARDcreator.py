12830e6ab0b4f63e6cea8a115d53587067809d5e
julienroy13 <jul.roy1311@gmail.com>
2017-10-23 19:57:33 -0400

    Added pop-up window which allows to enter a name for the newly created initialBOARD and end up the creation session

--- BOARDcreator.py
+++ BOARDcreator.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtGui, QtCore, QtWidgets
 import pyqtgraph as pg
 import sys
+import os
 import numpy as np
 
 
@@ -53,15 +54,15 @@ class BOARDcreator(QtWidgets.QWidget):
         self.SaveBtn = QtGui.QPushButton("Save")
         sizePolicyBtn.setHeightForWidth(self.SaveBtn.sizePolicy().hasHeightForWidth())
         self.SaveBtn.setSizePolicy(sizePolicyBtn)
-        self.SaveBtn.setStyleSheet("background-color: rgba(168, 237, 99,200); color: black; font-size: 32px; font: bold")
-        self.SaveBtn.clicked.connect(self.saveBOARD)
+        self.SaveBtn.setStyleSheet("background-color: rgba(17, 137, 107,200); color: black; font-size: 32px; font: bold; border-radius: 5px")
+        self.SaveBtn.clicked.connect(self.showDialog)
         self.SaveBtn.setEnabled(True)
         
         # IncreaseSize button
         self.IncreaseSizeBtn = QtGui.QPushButton("Increase Size")
         sizePolicyBtn.setHeightForWidth(self.IncreaseSizeBtn.sizePolicy().hasHeightForWidth())
         self.IncreaseSizeBtn.setSizePolicy(sizePolicyBtn)
-        self.IncreaseSizeBtn.setStyleSheet("background-color: gray; color: black; font-size: 16px; font: bold")
+        self.IncreaseSizeBtn.setStyleSheet("background-color: gray; color: black; font-size: 16px; font: bold; border-radius: 5px")
         self.IncreaseSizeBtn.clicked.connect(self.increase)
         self.IncreaseSizeBtn.setEnabled(True)
 
@@ -69,7 +70,7 @@ class BOARDcreator(QtWidgets.QWidget):
         self.DecreaseSizeBtn = QtGui.QPushButton("Decrease Size")
         sizePolicyBtn.setHeightForWidth(self.DecreaseSizeBtn.sizePolicy().hasHeightForWidth())
         self.DecreaseSizeBtn.setSizePolicy(sizePolicyBtn)
-        self.DecreaseSizeBtn.setStyleSheet("background-color: gray; color: black; font-size: 16px; font: bold")
+        self.DecreaseSizeBtn.setStyleSheet("background-color: rgba(200,200,200,250); color: rgba(150,150,150,250); font-size: 16px; font: bold; border-radius: 5px")
         self.DecreaseSizeBtn.clicked.connect(self.decrease)
         self.DecreaseSizeBtn.setEnabled(True)
 
@@ -91,7 +92,6 @@ class BOARDcreator(QtWidgets.QWidget):
     def getPos(self , event):
         y = int(event.pos().x())
         x = (self.BOARD.shape[0] - 1) - int(event.pos().y())
-        print("Click coordinates : ({0} ; {1})".format(x,y))
 
         if self.BOARD[x,y] == True:
             self.BOARD[x,y] = False
@@ -131,35 +131,36 @@ class BOARDcreator(QtWidgets.QWidget):
     def checkAllowedSizes(self):
         if self.currentSize == len(self.listOfSizes) - 1:
             self.IncreaseSizeBtn.setEnabled(False)
-            self.IncreaseSizeBtn.setStyleSheet("background-color: rgba(25,25,25,200); color: black; font-size: 16px; font: bold")
+            self.IncreaseSizeBtn.setStyleSheet("background-color: rgba(200,200,200,250); color: rgba(150,150,150,250); font-size: 16px; font: bold; border-radius: 5px")
         else:
             self.IncreaseSizeBtn.setEnabled(True)
-            self.IncreaseSizeBtn.setStyleSheet("background-color: gray; color: black; font-size: 16px; font: bold")
+            self.IncreaseSizeBtn.setStyleSheet("background-color: gray; color: black; font-size: 16px; font: bold; border-radius: 5px")
         
 
         if self.currentSize == 0:
             self.DecreaseSizeBtn.setEnabled(False)
-            self.DecreaseSizeBtn.setStyleSheet("background-color: rgba(25,25,25,200); color: black; font-size: 16px; font: bold")
+            self.DecreaseSizeBtn.setStyleSheet("background-color: rgba(200,200,200,250); color: rgba(150,150,150,250); font-size: 16px; font: bold; border-radius: 5px")
         else:
             self.DecreaseSizeBtn.setEnabled(True)
-            self.DecreaseSizeBtn.setStyleSheet("background-color: gray; color: black; font-size: 16px; font: bold")
+            self.DecreaseSizeBtn.setStyleSheet("background-color: gray; color: black; font-size: 16px; font: bold; border-radius: 5px")
 
 
     def updateImage(self):
         self.Image.setImage(image=np.transpose(np.flip(self.BOARD, axis=0)))
 
 
-    def saveBOARD(self):
-        print("TODO")
-        #np.save("NewBOARD", self.BOARD) # TODO : Get the user given name in the dialogue box
+    def showDialog(self):
+        # Shows a dialogue pop-up box to enter a name for the newly created initialBOARD
+        text, ok = QtWidgets.QInputDialog.getText(self, 'Final Step', 'Name (no spaces) :')
+        
+        if ok:
+            np.save(os.path.join("InitialBOARDs", text.replace(" ", "") + ".npy"), self.BOARD)
+            self.close()
 
 
-def run():
+# If we run the file directly
+if __name__ == '__main__':
     global app
     app = QtWidgets.QApplication(sys.argv)   # Every PyQt application must create an application object
-    gui = BOARDcreator()                        # Create an object "GuiManager"
+    gui = BOARDcreator()                     # Create an object "GuiManager"
     sys.exit(app.exec_())                    # Enter the main loop of the application
-
-
-if __name__ == '__main__':
-    run()
